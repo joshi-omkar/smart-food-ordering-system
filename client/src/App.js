c24927fe3a4eb8909f31d701b24d31bf9b2f6467
omkar <Omkar Joshi>
2023-04-29 15:07:22 +0530

    added Qr scanner

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
-import './App.css';
-import React from 'react'
-import Navbar from './Components/Navbar';
-import ConfirmOrder from './Components/User/ConfirmOrder';
-import WatingOderUserInfo from './Components/User/WatingOderUserInfo';
-import Menu from './Components/User/Menu';
-import ItemDetails from './Components/User/ItemDetails';
-import QrReaderComponent from './Components/User/QrReaderComponent';
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import ConfirmOrder from "./Components/User/ConfirmOrder";
+import WatingOderUserInfo from "./Components/User/WatingOderUserInfo";
+import Menu from "./Components/User/Menu";
+import ItemDetails from "./Components/User/ItemDetails";
+import QrReaderComponent from "./Components/User/QrReaderComponent";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      {/* <ConfirmOrder/> */}
-      <QrReaderComponent/>
-      {/* <WatingOderUserInfo/> */}
-      {/* <Menu/> */}
-      {/* <ItemDetails/> */}
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<QrReaderComponent />} />
+        <Route path="/menu" exact element={<Menu />} />
+        <Route path="/confirmOrder" exact element={<ConfirmOrder />} />
+        <Route
+          path="/watingOderUserInfo"
+          exact
+          element={<WatingOderUserInfo />}
+        />
+        <Route path="/itemDetails" exact element={<ItemDetails />} />
+      </Routes>
     </div>
   );
 }
--- client/src/Components/User/QrReaderComponent.js
+++ client/src/Components/User/QrReaderComponent.js
@@ -1,24 +1,83 @@
-import React, { useState } from "react";
-import { QrReader } from 'react-qr-reader';
-const QrReaderComponent = () => {
-    const [data, setData] = useState('No result');
+import React, { Component, useState } from "react";
+import QrReader from "modern-react-qr-reader";
+import { useNavigate } from "react-router-dom";
 
-    return (
-      <>
-        <QrReader
-          onResult={(result, error) => {
-            if (!!result) {
-              setData(result);
-            }
-  
-            if (!!error) {
-              console.info(error);
-            }
-          }}
-          style={{ width: '100%' }}
-        />
-        <p>{data}</p>
-      </>
+// class QrReaderComponent extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       result: "No result",
+//     };
+
+//     this.handleError = this.handleError.bind(this);
+//     this.handleScan = this.handleScan.bind(this);
+//   }
+
+//   navigate = useNavigate();
+
+//   handleScan = (data) => {
+//     if (data) {
+//       this.state.result = data;
+//       console.log(this.state.result);
+//       this.setState({ result: data });
+//     }
+//   };
+
+//   handleError = (err) => {
+//     console.error(err);
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <QrReader
+//           delay={300}
+//           facingMode={"environment"}
+//           onError={this.handleError}
+//           onScan={this.handleScan}
+//           style={{ width: "100%" }}
+//         />
+//         {this.state.result === "preorder"
+//           ? this.navigate("watingOderUserInfo")
+//           : this.navigate("menu")}
+//       </div>
+//     );
+//   }
+// }
+
+const QrReaderComponent = () => {
+
+  const [result, setResult] = useState('No result')
+
+  const navigate = useNavigate()
+
+  const handleScan = (data) =>{
+    if(data){
+      // result = data
+      setResult(data)
+    }
+  }
+
+  const handleError = (err) =>{
+    console.error(err);
+  }
+
+  return (
+    <div>
+      <QrReader
+        delay={300}
+        facingMode={"environment"}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: "100%" }}
+      />
+      {result === "prebook"
+        ? navigate("/watingOderUserInfo")
+        : navigate("/menu")}
+
+        {/* <p>{result}</p> */}
+    </div>
   );
 };
 
